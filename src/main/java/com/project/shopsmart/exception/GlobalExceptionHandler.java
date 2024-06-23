f676ebee58692b2ff0588f6ca03af4e93f65971c
Praneet <praneetdebnath@yahoo.com>
2024-06-23 16:10:01 +0530

    Handling DataIntegrityViolationException for Duplicate EmailID

---
+++ src/main/java/com/project/shopsmart/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.project.shopsmart.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return new ResponseEntity<>("Duplicate entry: Email already exists", HttpStatus.CONFLICT);
+    }
+}
